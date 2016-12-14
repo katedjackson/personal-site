c4b4513dd93a812a245091c0b7009fcd5bde3ccf
Kate <katelynj29@gmail.com>
2016-12-14 18:26:23 -0500

    updated app structure

--- browser/components/App.js
+++ browser/components/App.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default extends Component {
-  render(
+export default {
+  render() {
     return (
       <div>
         <Header />
         <ProjectGrid />
       </div>
     );
-  )
+  }
 }
---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(3000, function () {
+  console.log('Server listening on port', 3000);
+});
